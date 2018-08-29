e5a05c458df9ea8e3a7de7941a70b6d78fdca3c9
Ilhaan Rasheed <ilhaan@ilhaanrasheed.com>
2018-08-29 14:18:14 -0700

    Fix db name to use name from env_file

--- docker-entrypoint-initdb.d/init-tables.sh
+++ docker-entrypoint-initdb.d/init-tables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username dbuser --dbname web <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username dbuser --dbname webserver <<-EOSQL
         CREATE TABLE  weblogs (
                day    date,
                status varchar(3)
